de073a4be3a34592f9a276827a65215f100d5bf4
Gui-eng <76394241+Gui-eng@users.noreply.github.com>
2021-11-25 15:14:31 +0800

    Update main.c

--- test/main.c
+++ test/main.c
@@ -5,20 +5,12 @@ int main()
 {
     printf("Hello world!\n");
     printf("Philip Guiang");
-<<<<<<< HEAD
     printf("\nEmmanuel");
-=======
     printf("Emmanuel");
     printf("Jalen");
     printf("Dancel");
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> f2a164ddf5e650688e264d33911087aee4dcdab6
-=======
     printf ("Alex");
->>>>>>> 46e1db5557cfcf8879e8cd19d5986bd1664c0959
-=======
     printf("\nAldrin");
->>>>>>> origin/aldrin
+
     return 0;
 }
