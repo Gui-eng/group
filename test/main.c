f47a17a232541cfc5b53b267469be9773b2c8e20
Dredd0000 <jalenfajardo12@gmail.com>
2021-11-25 14:58:44 +0800

    My Name

--- test/main.c
+++ test/main.c
@@ -4,12 +4,10 @@
 int main()
 {
     printf("Hello world!\n");
-<<<<<<< HEAD
     printf("Philip Guiang");
-=======
     printf("Emmanuel");
->>>>>>> 2c3d932d5f83bff2087c888041b20cad7cefc36d
     printf("Jalen");
+    printf("Fajardo");
 
     return 0;
 }
